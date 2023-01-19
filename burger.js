b9b8ec92e2e765cac1a15ee9b4369c0c2dff6f2c
emammad22 <mammadoveltun22@gmail.com>
2023-01-19 21:41:13 +0400

    avoided burger and button problems

--- burger.js
+++ burger.js
@@ -7,7 +7,9 @@ burger.addEventListener('click', openMenu);
 function openMenu() {
     for (let i = 0; i < burger.children.length; i++) {
         burger.children[i].classList.toggle('burger-bar');
+        burger.classList.toggle('fixedBurger');
     }
 
     sidebar.classList.toggle('open');
+
 }
